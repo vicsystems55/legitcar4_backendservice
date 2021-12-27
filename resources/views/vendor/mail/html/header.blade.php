3abf922f37273c0d5327f27de179c39a2d7e1e1e
vicSystems <victorasuquob@gmail.com>
2021-12-27 03:18:49 +0100

    auth module

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Phoenixgn')
-<img src="https://legitcar.phoenixgn.com/legitcar2.png" class="logo" alt="LegitCar Logo">
+<img src="https://legitcar.phoenixgn.com/legit_car2.png" class="logo" alt="LegitCar Logo">
 @else
 {{ $slot }}
 @endif
